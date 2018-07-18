e3aecaf5cf2c3d6411fa5c8b40901d7cb2e5949a
王宏韬 <wanghongtaoaaa@qq.com>
2018-07-18 14:34:38 +0800

    appfront update

---
+++ tutorial/appfront/src/components/FaultDetection.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="detection">
+    <div class="head-left">
+  		<span class="logo">光伏智能运维系统</span>
+  	</div>
+
+  	<div class="head-right">
+  	   <span class="nav">
+  		  <ul>
+  			   <li><a href="#">系统设置</a></li>
+  			   <li><a href="#">故障告警</a></li>
+  			   <li><a href="#">数据分析</a></li>
+  			   <li><a href="#">运行监测</a></li>
+  			   <li><a href="#">首    页</a></li>
+  		  </ul>
+  	   </span>
+  	   <div class="clearfloat"></div>
+    </div>
+
+  <div class="left">
+  	<div class="row">
+  		<div class="row"><span class="name">名称</span></div>
+  		<div class="row"><span class="capacity">容量</span></div>
+  		<div class="row"><span class="area">地区</span></div>
+  		<div class="row"><span class="station">状态</span></div>
+  	</div>
+  	<div class="shuru">
+  		<form>
+  		<input type="text" name="text" size=35><br>
+  		</form>
+  	</div>
+  	<div class="mulu1">北京光伏电站</div>
+  	<div class="mulu2">上海光伏电站</div>
+  </div>
+    <div class="right">
+  	  <img src="./images/u1141.png" alt="image1" width="100%">
+      <comList></comList>
+    </div>
+  </div>
+</template>
+
+<script>
+  import ComList from './el-simple-com/com-list.vue'
+
+  export default {
+    name: 'faultdetection',
+    components: {
+      comList: ComList
+    },
+  }
+</script>
+
+<style>
+    .detection{
+        margin:0; padding:0; font-size:30px; font-weight:bold
+    }
+
+    .head-left{
+        width:20%;height:100px;background:#9CF;float:left
+    }
+
+    .head-right{
+        width:80%;height:100px;background:#9CF;float:right
+    }
+
+    .left{
+        width:20%; height:1000px; background:whitesmoke; float:left
+    }
+
+    .right{
+        width:80%; height:1000px;background:whitesmoke; float:right
+    }
+
+    .logo{
+        width:auto;line-height:100px;margin-left:10px;
+    }
+
+    .nav{
+        width:80%;
+    }
+
+    .nav ul{
+        list-style:none;
+    }
+
+    .nav li{
+        float:right;padding:10px;
+    }
+
+    .clearfloat{
+        clear:both
+    }
+
+    .nav ul li a{
+        text-decoration:none;color:black;
+    }
+
+    .nav ul li a:hover{
+        color:white;background:black;
+    }
+
+    .row {
+        float:left;
+        padding:5px;
+    }
+
+    .mulu1{
+        margin-left:10px;
+    }
+
+    .mulu2{
+        margin-left:10px;
+    }
+
+    .shuru{
+        margin-left:10px;
+    }
+</style>
---
+++ tutorial/appfront/src/components/el-simple-com/com-list.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-row>
+    <!-- tableData数据的映射 -->
+    <el-table :data="tableData"
+    border
+    style="width: 100%">
+      <el-table-column
+      v-for="{ prop, label } in tabConfigs"
+      :key="prop"
+      :prop="prop"
+      :width="280"
+      :label="label">
+      </el-table-column>
+    </el-table>
+    <!-- <el-row>
+      <div class="block">
+        <el-pagination
+          @current-change="handleCurrentChange"
+          layout="prev, pager, next"
+          :page-size="pageSize"
+          :total="1000">
+        </el-pagination>
+      </div>
+    </el-row> -->
+  </el-row>
+</template>
+
+<script>
+  export default {
+    name: 'ComList',
+    data() {
+      this.tabConfigs = [
+        {prop: 'dev_name', label: '设备名称'},
+        {prop: 'dev_avg_p', label: '当前功率（w）'},
+        {prop: 'dev_day_w', label: '今日发电量（kw*h）'},
+        {prop: 'dev_effect_time', label: '当日的等效小时（h）'},
+        {prop: 'dev_status', label: '采集器的状态'},
+        {prop: 'dev_get_time', label: '数据采集的时间'},
+      ]
+      return {
+        tableData: []
+        // pageSize: 2
+      }
+    },
+    //mounted为vue对象的生命周期
+    mounted: function() {
+      this.showAll()
+    },
+    methods: {
+      //通过异步请求，ajax用来获取数据
+      showAll(){
+        this.$ajax.get('http://127.0.0.1:8000/pv/get/detection/2018/1/18/')
+        .then(function (response) {
+          for (var i = 0; i < response.data.results.length; i++) {
+            console.log(response.data.results[i])
+            this.setTableData(response.data.results[i])
+          }
+        }.bind(this))
+        .catch(function (error) {
+        });
+        console.log(this.tableData);
+      },
+      //设置tableData对象
+      setTableData(result){
+        var effect_time = result.time_sum/60
+        var avg_p = result.p_avg
+        var day_w = result.p_avg * result.time_sum/60/100
+        var element = {
+          dev_name: result.cityid + ' ' + result.pcbid,
+          dev_avg_p: avg_p.toFixed(2),
+          dev_day_w: day_w.toFixed(4),
+          dev_effect_time: effect_time.toFixed(2),
+          dev_status: '未知',
+          dev_get_time: result.time_min+' 到 '+result.time_max
+        }
+        this.tableData.push(element)
+      },
+    }
+  }
+</script>
+<style>
+
+</style>
