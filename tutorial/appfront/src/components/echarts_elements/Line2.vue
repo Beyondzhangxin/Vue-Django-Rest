4615f0001754fe4b9ba606e862033b23a5f32f39
王宏韬 <wanghongtaoaaa@qq.com>
2018-07-24 14:59:26 +0800

    clear version 5

--- tutorial/appfront/src/components/echarts_elements/Line2.vue
+++ tutorial/appfront/src/components/echarts_elements/Line2.vue
@@ -7,6 +7,7 @@
   let echarts = require('echarts/lib/echarts')
 
   require('echarts/lib/chart/bar')
+  require('echarts/lib/chart/line')
   // 引入提示框和title组件
   require('echarts/lib/component/tooltip')
   require('echarts/lib/component/title')
