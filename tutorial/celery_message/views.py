f30da3674ab89e53110e06dbae67637fc3b27944
Jason <573329319@qq.com>
2018-07-24 11:42:50 +0800

    beginning

--- tutorial/celery_message/views.py
+++ tutorial/celery_message/views.py
@@ -21,22 +21,3 @@ def mul_num(request, format='json'):
         dict = request.query_params.dict()
         res = mul.delay(int(dict['num1']), int(dict['num2']))
         return Response(data=json.loads('{"res":' + str(res.get()) + '}'), content_type='/json')
-#this is another change from jason branch
-
-
-##this is from jason branch
-#i'M JASON  I want to do more
-
-
-
-
-#ok I have fixed the bugs ,Now it's time to return where I started!
-
-
-
-
-
-#this is the bug branch ,I am doing the bugs!
-
-# ok I have finished , now back to the jason!
-#return to the jason branch, now I'm going to the bug branch!
