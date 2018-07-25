8d269d3324f732174250f8a4dedb3af94dbbef78
Guan <13194450+Alanwell@users.noreply.github.com>
2018-07-25 14:54:31 +0800

    添加elpow文件在Power文件夹下

--- tutorial/appfront/src/components/power/Power.vue
+++ tutorial/appfront/src/components/power/Power.vue
@@ -1,40 +1,85 @@
 <template>
   <div class="Power">
-    <div id="pheader">
-      <div id="">123</div>
-      <div id="">213</div>
-    </div>
-    <div id="pmain">
-      <el-row>
-        <el-col :span="12"><div class="grid-content1 bg-purple">北京光伏电站</div></el-col>
-        <el-col :span="12"><div class="grid-content1 bg-purple-light">上海光伏电站</div></el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="6"><div class="grid-content2 bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content2 bg-purple">
-          <div id="text">
-          <div class="text1">总装机容量：</div><br>
-          <div class="text2">当前运行功率：</div><br>
-          <div class="text3">当日有效时数：</div><br>
-          <div class="text4">当月有效时数：</div><br>
-          </div>
-        </div></el-col>
-        <el-col :span="6"><div class="grid-content2 bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content2 bg-purple">
-          <div id="text">
-          <div class="text1">总装机容量：</div><br>
-          <div class="text2">当前运行功率：</div><br>
-          <div class="text3">当日有效时数：</div><br>
-          <div class="text4">当月有效时数：</div><br>
-          </div>
-        </div></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="12"><div class="grid-content3 bg-purple">正常</div></el-col>
-        <el-col :span="12"><div class="grid-content3 bg-purple-light">正常</div></el-col>
-      </el-row>
-    </div>
-    </div>
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="4" v-for="item in items[0]">{{ item.key+item.value }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="2">电站状态</el-col>
+          <el-col :span="2">
+            <div>全部</div>
+          </el-col>
+          <el-col :span="2">
+            <div>正常</div>
+          </el-col>
+          <el-col :span="2">
+            <div>异常</div>
+          </el-col>
+          <el-col :span="2">
+            <div>离线</div>
+          </el-col>
+          <el-col :span="2">
+            <div>停机</div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <el-container>
+          <el-header class="mh">
+            <div class="row">
+              <el-row>
+                <el-col :span="3">当日有效使数:</el-col>
+                <el-col :span="4">
+                  <div>小时</div>
+                </el-col>
+                <el-col :span="3">
+                  <input>
+
+                  </input>
+                </el-col>
+                <el-col :span="1">
+                  ~
+                </el-col>
+                <el-col :span="3">
+                  <input>
+
+                  </input>
+                </el-col>
+                <el-col :span="3">
+                  <div>小时</div>
+                </el-col>
+              </el-row>
+            </div>
+            <div class="row">
+              <el-row>
+                <el-col :span="3">电站所在地区</el-col>
+                <el-col :span="7">
+                  <el-select
+                    v-model="value10"
+                    multiple
+                    filterable
+                    allow-create
+                    default-first-option
+                    placeholder="请选择地区">
+                    <el-option
+                      v-for="item in options5"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value">
+                    </el-option>
+                  </el-select>
+                </el-col>
+              </el-row>
+            </div>
+          </el-header>
+          <el-main class="mm">
+            
+
+          </el-main>
+        </el-container>
+      </el-main>
+    </el-container>
   </div>
 </template>
 <script>
@@ -45,69 +90,102 @@ import Line2 from '../echarts_elements/Line2'
 export default {
   name : 'Power',
   components: {
-    
   },
   data () {
     return {
-     
+      msg: "Hello world",
+      items: [
+        [
+          {
+            key: "电站总数:",
+            value: 2+"座",
+          },
+          {
+            key: "总装机容量:",
+            value: 660.66+"kWp",
+          },
+          {
+            key: "今日总发电量:",
+            value: 3038.26+"kWh",
+          },
+          {
+            key: "累计总发电量:",
+            value: 36.22+"万kWh",
+          },
+        ],
+      ],
+      options5: [
+        {
+          value: '全部',
+          label: '全部'
+        },{
+          value: '北京',
+          label: '北京'
+        },{
+          value: '上海',
+          label: '上海'
+        },{
+          value: '深圳',
+          label: '深圳'
+        },
+      ],
+        value10: []
     }
   }
 }
 </script>
 <style scoped>
+  .el-container {
+    height: 100%;
+  }
+  .el-row {
+    display: block;
+    height: 50%;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    display: inline-block;
+    border: 1px solid black;
+  }
+
   .Power {
     height: 100%;
-    width: 100%;
   }
-  #pheader {
-    height: 100px;
+
+  .el-main {
+    height: 80%;
   }
-  #pheader div{
-    margin: 0 auto;
-    padding: 6px 0 8px;
+
+  .el-select {
+
+  }
+
+  .mh {
+    border: 2px solid black;
+  }
+
+  .mm {
+    border: 2px solid black;
+    border-top: 0px;
+  }
+
+  .row .el-row{
+
+  }
+
+  input {
+    width: 50px
+
+  }
+
+
+  #plist {
+
+  }
+  #li {
     float: left;
+    display: inline-block;
   }
-  #pmain{
-    height: 400px;
-    background-color: white;
-  }
-
- .el-row {
-    margin-bottom: 20px;
-  }
-
-  .el-col {
-    border-radius: 4px;
-  }
-
-.bg-purple {
-    background: #d3dce6;
-  }
-
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-
-  .grid-content1 {
-    border-radius: 4px;
-    min-height: 40px;
-    padding-top:10px;
-  }
-
- .grid-content2 {
-    border-radius: 4px;
-    min-height: 200px;
-    margin-top:20px;
-  }
-
-.grid-content3{
-    border-radius: 4px;
-    min-height: 50px;
-    margin-top:20px;
-    padding-top:10px;
-}
-
-#text{
-  float:left;margin-top:20px;margin-left:10px;
-}
 </style>
