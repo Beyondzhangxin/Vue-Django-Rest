6507259289168c795f6a3f1127c9e22155521838
Jason <573329319@qq.com>
2018-07-30 19:33:06 +0800

    clear version 5

--- tutorial/appfront/src/components/home/aside.vue
+++ tutorial/appfront/src/components/home/aside.vue
@@ -4,42 +4,55 @@
       列表名称
     </div>
     <div class="aside1">
-      <div><el-button class="b" size="mini" type="primary"
-         @click="clickButtom(buttoms[0])">{{ buttoms[0] }}</el-button></div>
-      <div><el-button class="b" size="mini" type="success"
-         @click="clickButtom(buttoms[1])">{{ buttoms[1] }}</el-button></div>
-      <div><el-button class="b" size="mini" type="warning"
-         @click="clickButtom(buttoms[2])">{{ buttoms[2] }}</el-button></div>
-      <div><el-button class="b" size="mini" type="danger"
-         @click="clickButtom(buttoms[3])">{{ buttoms[3] }}</el-button></div>
+      <div>
+        <el-button class="b" size="mini" type="primary"
+                   @click="clickButtom(buttoms[0])">{{ buttoms[0] }}
+        </el-button>
+      </div>
+      <div>
+        <el-button class="b" size="mini" type="success"
+                   @click="clickButtom(buttoms[1])">{{ buttoms[1] }}
+        </el-button>
+      </div>
+      <div>
+        <el-button class="b" size="mini" type="warning"
+                   @click="clickButtom(buttoms[2])">{{ buttoms[2] }}
+        </el-button>
+      </div>
+      <div>
+        <el-button class="b" size="mini" type="danger"
+                   @click="clickButtom(buttoms[3])">{{ buttoms[3] }}
+        </el-button>
+      </div>
     </div>
     <div class="aside3">
       <el-input
-      placeholder="输入关键字进行过滤"
-      v-bind:placeholder="placeholder"
-      v-model="filterText">
+        placeholder="输入关键字进行过滤"
+        v-bind:placeholder="placeholder"
+        v-model="filterText">
       </el-input>
       <el-tree
-      class="filter-tree"
-      :data="data2"
-      :props="defaultProps"
-      default-expand-all
-      show-checkbox
-      node-key="id"
-      :filter-node-method="filterNode"
-      @check="sendTree()"
-      ref="tree2">
+        class="filter-tree"
+        :data="data2"
+        :props="defaultProps"
+        default-expand-all
+        show-checkbox
+        node-key="id"
+        :filter-node-method="filterNode"
+        @check="sendTree()"
+        ref="tree2">
       </el-tree>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'home_aside',
-  props: ['request'] ,
-  data () {
-    return {
+  export default {
+    name: 'home_aside',
+    props: ['request'],
+    data () {
+
+      return {
         buttoms: ['名称', '容量', '地区', '状态'],
         placeholder: "输入关键字进行过滤",
         filterText: '',
@@ -62,40 +75,52 @@ export default {
           children: 'children',
           label: 'label'
         }
-    }
-  },
-  mounted: function() {
-    //this.load();
-  },
-  watch: {
-     filterText(val) {
-       this.$refs.tree2.filter(val);
-     }
-   },
+      }
+    },
+    mounted: function () {
+      //this.load();
+        this.transdata();
+    },
+    watch: {
+      filterText(val) {
+        this.$refs.tree2.filter(val);
+      }
+    },
 
-   methods: {
-     sendTree(){
-       console.log(this.$refs.tree2.getCheckedKeys());
-     },
+    methods: {
+      transdata(){
+        var response = this.$http.get('http://127.0.0.1:8000/system/powerStations');
+        var list=[];
+        for (item in response.list) {
+            var jsonObj= new Object();
+            jsonObj
+        }
 
-     filterNode(value, data) {
-       if (!value) return true;
-       return data.label.indexOf(value) !== -1;
-     },
-     load(){
-       this.$ajax.get(this.data)
-       .then(function (response) {
-         console.log(response)
-       }.bind(this))
-       .catch(function (error) {
-       });
-     },
-     clickButtom(str){
-       console.log(23);
-       this.placeholder = "输入"+ str +"进行过滤";
-     },
-   },
-}
+        this.data2 = list;
+
+      },
+      sendTree(){
+        console.log(this.$refs.tree2.getCheckedKeys());
+      },
+
+      filterNode(value, data) {
+        if (!value) return true;
+        return data.label.indexOf(value) !== -1;
+      },
+      load(){
+        this.$ajax.get(this.data)
+          .then(function (response) {
+            console.log(response)
+          }.bind(this))
+          .catch(function (error) {
+          });
+      },
+      clickButtom(str){
+        console.log(23);
+        this.placeholder = "输入" + str + "进行过滤";
+      },
+    },
+  }
 </script>
 <style scoped>
   .aside {
@@ -107,22 +132,23 @@ export default {
     box-sizing: content-box;
   }
 
-.aside0{
-  margin-bottom:5px;
-  margin-top:4px;
-  margin-left:-80px;
-}
-.aside1 {
+  .aside0 {
+    margin-bottom: 5px;
+    margin-top: 4px;
+    margin-left: -80px;
+  }
+
+  .aside1 {
     width: 100%;
     height: 40px;
     display: block;
-}
+  }
 
-.b{
-  padding-right:1px;
-  text-align:center;
+  .b {
+    padding-right: 1px;
+    text-align: center;
 
-}
+  }
 
   .aside3 {
     width: 80%;
@@ -138,18 +164,18 @@ export default {
 
   .el-button {
     float: left;
-     padding-right: 15px;
-     position: relative;
-     margin-left: 1px;
-     top: 4px;
-     z-index: 5;
-     display: inline-block;
-     border-top-left-radius: 10px 10px;
-     border-top-right-radius: 10px 10px;
+    padding-right: 15px;
+    position: relative;
+    margin-left: 1px;
+    top: 4px;
+    z-index: 5;
+    display: inline-block;
+    border-top-left-radius: 10px 10px;
+    border-top-right-radius: 10px 10px;
   }
 
   .b1 {
-    border-left-color:  #409eff;
+    border-left-color: #409eff;
     border-top-left-radius: 10px 7px;
     border-top-right-radius: 10px 7px;
   }
--- tutorial/system/urls.py
+++ tutorial/system/urls.py
@@ -2,15 +2,12 @@ from django.conf.urls import url
 from . import views
 
 app_name = "system"
-urlpatterns=[
-    url(r'^psNum/$',views.powerstationNumber),
-
-
-
+urlpatterns = [
     # url(r'^postTest1/$', views.postTest1),
     # url(r'^postTest2/$', views.postTest2),
     # url(r'^$',views.index,name='index'),
     # url(r'^(?P<p2>\d+)/(?P<p3>\d+)/(?P<p1>\d+)/$',views.detail,name='detail'),
-    url(r'test$',views.apiTest,),
+    url(r'test$', views.apiTest, ),
+    url(r'powerStations', views.powerStations),
 
 ]
