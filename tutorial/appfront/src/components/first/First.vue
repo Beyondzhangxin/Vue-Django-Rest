6530d2fa8c7d5ffd3e16e8c04a12ed5136f0bc77
王宏韬 <wanghongtaoaaa@qq.com>
2018-07-31 15:45:25 +0800

    first.vue update

--- tutorial/appfront/src/components/first/First.vue
+++ tutorial/appfront/src/components/first/First.vue
@@ -164,14 +164,9 @@
               <Line2 v-bind="settings.l1"></Line2>
             </div>
             <div class="fm2">
-
+              <Line2 v-bind="settings.l2"></Line2>
             </div>
-
-
           </div>
-
-
-
       </el-main>
   </el-container>
   </div>
@@ -285,40 +280,39 @@ export default {
         l1:{
           id: 'line1',
           option:{
-    title: {
-        text: '动态数据',
-        subtext: '纯属虚构'
-    },
-    tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-            type: 'cross',
-            label: {
+            title: {
+              text: '当日发电量',
+            },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
                 backgroundColor: '#283b56'
+              }
             }
-        }
-    },
-    legend: {
-        data:['最新成交价', '预购队列']
-    },
-    toolbox: {
-        show: true,
-        feature: {
-            dataView: {readOnly: false},
-            restore: {},
-            saveAsImage: {}
-        }
-    },
-    dataZoom: {
-        show: false,
-        start: 0,
-        end: 100
-    },
-    xAxis: [
-        {
-            type: 'category',
-            boundaryGap: true,
-            data: (function (){
+          },
+          legend: {
+            data:['逆变器发电量', '电能表发电量', '理论发电量']
+          },
+          toolbox: {
+            show: true,
+            feature: {
+              dataView: {readOnly: false},
+              restore: {},
+              saveAsImage: {}
+            }
+          },
+          dataZoom: {
+            show: false,
+            start: 0,
+            end: 100
+          },
+          xAxis: [
+            {
+              type: 'category',
+              boundaryGap: true,
+              data: (function (){
                 var now = new Date();
                 var res = [];
                 var len = 10;
@@ -327,9 +321,9 @@ export default {
                     now = new Date(now - 2000);
                 }
                 return res;
-            })()
-        },
-        {
+              })()
+          },
+          {
             type: 'category',
             boundaryGap: true,
             data: (function (){
@@ -340,29 +334,29 @@ export default {
                 }
                 return res;
             })()
-        }
-    ],
-    yAxis: [
-        {
+          }
+        ],
+        yAxis: [
+          {
             type: 'value',
             scale: true,
-            name: '价格',
-            max: 30,
+            name: 'kWh',
+            max: 500,
             min: 0,
             boundaryGap: [0.2, 0.2]
-        },
-        {
+          },
+          {
             type: 'value',
             scale: true,
             name: '预购量',
             max: 1200,
             min: 0,
             boundaryGap: [0.2, 0.2]
-        }
-    ],
-    series: [
-        {
-            name:'预购队列',
+          }
+        ],
+        series: [
+          {
+            name:'逆变器发电量',
             type:'bar',
             xAxisIndex: 1,
             yAxisIndex: 1,
@@ -374,25 +368,160 @@ export default {
                 }
                 return res;
             })()
-        },
-        {
-            name:'最新成交价',
-            type:'line',
+          },
+          {
+            name:'电能表发电量',
+            type:'bar',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
             data:(function (){
                 var res = [];
-                var len = 0;
-                while (len < 10) {
-                    res.push((Math.random()*10 + 5).toFixed(1) - 0);
-                    len++;
+                var len = 10;
+                while (len--) {
+                    res.push(Math.round(Math.random() * 1000));
                 }
                 return res;
             })()
-        }
-    ]
-},
-        },
+          },
+          {
+            name:'理论发电量',
+            type:'line',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            data:(function (){
+                var res = [];
+                var len = 10;
+                while (len--) {
+                    res.push(Math.round(Math.random() * 1000));
+                }
+                return res;
+            })()
+          },
+        ]
+      },
+      },
         l2:{
-
+          id: 'line2',
+          option: {
+            title: {
+              text: '当日发电量',
+            },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#283b56'
+              }
+            }
+          },
+          legend: {
+            data:['逆变器发电功率', '电能表发电功率', '总辐照度']
+          },
+          toolbox: {
+            show: true,
+            feature: {
+              dataView: {readOnly: false},
+              restore: {},
+              saveAsImage: {}
+            }
+          },
+          dataZoom: {
+            show: false,
+            start: 0,
+            end: 100
+          },
+          xAxis: [
+            {
+              type: 'category',
+              boundaryGap: true,
+              data: (function (){
+                var now = new Date();
+                var res = [];
+                var len = 10;
+                while (len--) {
+                    res.unshift(now.toLocaleTimeString().replace(/^\D*/,''));
+                    now = new Date(now - 2000);
+                }
+                return res;
+              })()
+          },
+          {
+            type: 'category',
+            boundaryGap: true,
+            data: (function (){
+                var res = [];
+                var len = 10;
+                while (len--) {
+                    res.push(10 - len - 1);
+                }
+                return res;
+            })()
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            scale: true,
+            name: 'kWh',
+            max: 500,
+            min: 0,
+            boundaryGap: [0.2, 0.2]
+          },
+          {
+            type: 'value',
+            scale: true,
+            name: '预购量',
+            max: 1200,
+            min: 0,
+            boundaryGap: [0.2, 0.2]
+          }
+        ],
+        series: [
+          {
+            name:'逆变器发电功率',
+            type:'line',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            data:(function (){
+                var res = [];
+                var len = 10;
+                while (len--) {
+                    res.push(Math.round(Math.random() * 1000));
+                }
+                return res;
+            })()
+          },
+          {
+            name:'电能表发电功率',
+            type:'line',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            data:(function (){
+                var res = [];
+                var len = 10;
+                while (len--) {
+                    res.push(Math.round(Math.random() * 1000));
+                }
+                return res;
+            })()
+          },
+          {
+            name:'总辐照度',
+            type:'line',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            data:(function (){
+                var res = [];
+                var len = 10;
+                while (len--) {
+                    res.push(Math.round(Math.random() * 1000));
+                }
+                return res;
+            })()
+          },
+        ]
+      },
 
         }
       }
